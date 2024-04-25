686f5a09e74573b41f0ced791ea1511fbb5c01a4
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2024-04-25 19:56:42 +0100

    fix: build.rs ci issue

--- build.rs
+++ build.rs
@@ -1,10 +1,6 @@
-use std::{env, error::Error, path::PathBuf};
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Put the linker script somewhere the linker can find it
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    println!("cargo:rustc-link-search={}", out.display());
-
     // Only re-run the build script when build.rs is changed - aka never
     println!("cargo:rerun-if-changed=build.rs");
 
