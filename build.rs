38a06b6a145eb86957aecfbe8f03db5fd22da2c6
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2024-06-22 17:22:55 +0100

    pr: Display Buttons on screen (#3)
    + deps: format Cargo.toml
    + cargo update
    + display play/pause/skip buttons on screen
    + add buttons.rs for button tasks
    + simplify blink
    + refactor display.rs, add structs and improve speed
    + add a potentiometer.rs file with abstractions
    + add signals for volume, play/pause and skip

--- build.rs
+++ build.rs
@@ -2,10 +2,10 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Only re-run the build script when build.rs is changed - aka never
-    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=build.rs,.env");
 
-    #[cfg(feature = "defmt")]
-    println!("cargo:rustc-link-arg=-Tdefmt.x");
+    // #[cfg(feature = "defmt")]
+    // println!("cargo:rustc-link-arg=-Tdefmt.x");
 
     #[cfg(feature = "net")]
     println!("cargo:rustc-link-arg=-Trom_functions.x");
